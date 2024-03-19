a8ef6c2ae8e28fab4c6f4cc1fc4c52986da55eaa
GiohAlss <giovanna.almeida074@gmail.com>
2024-03-19 20:34:54 -0300

    Banco finalizado.

---
+++ bd-at3-trigger-giovanna.sql
@@ -0,0 +1,21 @@
+use bd_aula;
+
+delimiter $
+
+create trigger trg_delete_alunos before delete on tbl_alunos
+for each row 
+begin
+insert into tbl_alunos_bkp 
+set
+	cod_aluno = old.cod_aluno,
+	cod_turma = old.cod_turma,
+	nome_aluno = old.nome_aluno,
+	cpf = old.cpf,
+	rg = old.rg,
+	telefone_aluno = old.telefone_aluno,
+	telefone_responsavel = old.telefone_responsavel,
+	email = old.email,
+	data_nascimento = old.data_nascimento,
+	date_delete = now();
+end
+$
